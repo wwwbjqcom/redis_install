9e7f5c6c126aad09f5c8215d416a90717ab6aec6
wwwbjqcom <wwwbjqcom@126.com>
2016-09-01 11:46:24 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/bin/env python
+# -*- encoding: utf-8 -*-
+################################################
+import lib.entrance as entrance
+
+
+
+if __name__ == "__main__":
+        entrance.entrance()
